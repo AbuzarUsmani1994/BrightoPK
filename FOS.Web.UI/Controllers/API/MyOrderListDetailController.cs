84a9f80b907f76cbd63b00b040e26de2a4948212
AbuzarUsmani1994 <69624982+AbuzarUsmani1994@users.noreply.github.com>
2021-07-07 11:26:28 +0500

    Add project files.

---
+++ FOS.Web.UI/Controllers/API/MyOrderListDetailController.cs
@@ -0,0 +1,79 @@
+﻿using FOS.DataLayer;
+using FOS.Setup;
+using Shared.Diagnostics.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace FOS.Web.UI.Controllers.API
+{
+    public class MyOrderListDetailController : ApiController
+    {
+        FOSDataModel db = new FOSDataModel();
+
+        public IHttpActionResult Get(int JobID)
+        {
+            List<Sp_MyOrderListDetail_Result> result = new List<Sp_MyOrderListDetail_Result>();
+            FOSDataModel dbContext = new FOSDataModel();
+            try
+            {
+                if (JobID > 0)
+                {
+                    object[] param = { JobID };
+
+                    var Reason = db.JobsDetails.Where(x => x.JobID == JobID).FirstOrDefault();
+                    if (Reason.VisitPurpose == "Ordering")
+                    {
+
+
+                         result = dbContext.Sp_MyOrderListDetail(JobID).ToList();
+                        if (result != null && result.Count > 0)
+                        {
+                            return Ok(new
+                            {
+                                MyOrderListDetail = result
+
+                            });
+                        }
+                    }
+                    else
+                    {
+                        var name = db.Tbl_FollowupReasons.Where(x => x.ID == Reason.FollowupReason).Select(x=> new {
+                            JobID=x.ID,
+                            ItemName= x.Name,
+                            Quantity=0,
+
+                        }).FirstOrDefault();
+                        if (name != null )
+                        {
+                            object[] stringArray = new object[] { name };
+                            return Ok(new
+                            {
+                                MyOrderListDetail = stringArray
+
+                            });
+                        }
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Instance.Error(ex, "VisitDetailController GET API Failed");
+            }
+            object[] paramm = {};
+            return Ok(new
+            {
+                MyOrderListDetail = paramm
+            });
+
+        }
+
+
+    }
+}
---
+++ Shared/OrderSummeryReportData.cs
@@ -0,0 +1,56 @@
+﻿using FOS.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOS.Shared
+{
+    public class OrderSummaryReportData
+    {
+
+        public int RetailerID { get; set; }
+        public string ShopName { get; set; }
+        public string DateFrom { get; set; }
+        public string DateTo { get; set; }
+        public int DistributorID { get; set; }
+        public int RangeID { get; set; }
+        public int RegionID { get; set; }
+        public int CityID { get; set; }
+        public int SaleOfficerID { get; set; }
+        public string Email { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public List<CategoryData> range { get; set; }
+        public List<MainCategories> ranges { get; set; }
+        public List<SaleOfficerData> saleofficerdata { get; set; }
+        public virtual List<SaleOfficer> SaleOfficer { get; set; }
+        public List<RetailerData> dealerdata { get; set; }
+        public List<CityData> CityData { get; set; }
+        public virtual List<RegionalHeadData> RegionalHead { get; set; }
+        public List<RegionData> regionData { get; set; }
+    }
+
+
+
+
+    public class KPIData
+    {
+
+        public int SOID { get; set; }
+        public string SoName { get; set; }
+        public string RHName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string ElapseTime { get; set; }
+        public int? totalVisits { get; set; }
+        public int ProductiveShops { get; set; }
+        public int NonProductive { get; set; }
+        public decimal ProductivePer { get; set; }
+        public double perDayorders { get; set; }
+        public int? totallines { get; set; }
+        public string Linesperbill { get; set; }
+      
+    }
+}
